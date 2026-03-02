821dece7c7951f60711e7eebc4b0ae2df3eac8c2
Wisllor PS <79232049+Kevallion@users.noreply.github.com>
2026-03-02 12:45:35 +0100

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-06ZYF4G9Z6"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-06ZYF4G9Z6');
+          `}
+        </Script>
         <Script
           id="gtm"
           strategy="beforeInteractive"
